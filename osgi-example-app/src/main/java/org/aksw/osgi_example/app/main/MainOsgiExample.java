e0e1ca532fdf33f95c3506a169637b4451a19751
Claus Stadler <RavenArkadon@googlemail.com>
2016-11-24 19:37:15 +0100

    formatting

--- osgi-example-app/src/main/java/org/aksw/osgi_example/app/main/MainOsgiExample.java
+++ osgi-example-app/src/main/java/org/aksw/osgi_example/app/main/MainOsgiExample.java
@@ -17,60 +17,60 @@ import org.osgi.framework.launch.Framework;
 import org.osgi.framework.launch.FrameworkFactory;
 
 public class MainOsgiExample {
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		try {
-			FrameworkFactory frameworkFactory = ServiceLoader.load(FrameworkFactory.class).iterator().next();
+        try {
+            FrameworkFactory frameworkFactory = ServiceLoader.load(FrameworkFactory.class).iterator().next();
 
-			Map<String, String> config = new HashMap<String, String>();
-			config.put(Constants.FRAMEWORK_STORAGE_CLEAN, Constants.FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
+            Map<String, String> config = new HashMap<String, String>();
+            config.put(Constants.FRAMEWORK_STORAGE_CLEAN, Constants.FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
 
-			config.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA,
-					String.join(",", "org.aksw.osgi_example.api;version=\"1.0.0\""));
+            config.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA,
+                    String.join(",", "org.aksw.osgi_example.api;version=\"1.0.0\""));
 
-			List<File> jarFiles = Arrays.asList("a", "b").stream().map(implStr -> {
-				String jarPathStr = String
-						.format("../osgi-example-impl-%1$s/target/osgi-example-impl-%1$s-1.0.0-SNAPSHOT.jar", implStr);
-				File jarFile = new File(jarPathStr);
-				return jarFile;
-			}).collect(Collectors.toList());
+            List<File> jarFiles = Arrays.asList("a", "b").stream().map(implStr -> {
+                String jarPathStr = String
+                        .format("../osgi-example-impl-%1$s/target/osgi-example-impl-%1$s-1.0.0-SNAPSHOT.jar", implStr);
+                File jarFile = new File(jarPathStr);
+                return jarFile;
+            }).collect(Collectors.toList());
 
-			Framework framework = frameworkFactory.newFramework(config);
-			try {
-				framework.init();
-				framework.start();
-				BundleContext context = framework.getBundleContext();
+            Framework framework = frameworkFactory.newFramework(config);
+            try {
+                framework.init();
+                framework.start();
+                BundleContext context = framework.getBundleContext();
 
-				for (File jarFile : jarFiles) {
-					String jarFileStr = jarFile.getAbsolutePath();
-					System.out.println("Loading: " + jarFileStr);
+                for (File jarFile : jarFiles) {
+                    String jarFileStr = jarFile.getAbsolutePath();
+                    System.out.println("Loading: " + jarFileStr);
 
-					Bundle bundle = context.installBundle("reference:file:" + jarFileStr);
-					try {
-						bundle.start();
+                    Bundle bundle = context.installBundle("reference:file:" + jarFileStr);
+                    try {
+                        bundle.start();
 
-						ServiceReference<GreetingService> sr = context.getServiceReference(GreetingService.class);
-						if (sr == null) {
-							throw new RuntimeException("Service reference is null");
-						}
-						GreetingService service = context.getService(sr);
-						String msg = service.getGreetingMessage("Anne");
-						System.out.println("RESULT: " + msg);
-					} finally {
-						bundle.stop();
-						bundle.uninstall();
-					}
-				}
-			} finally {
-				framework.stop();
-				framework.waitForStop(0);
-			}
-			System.out.println("Done.");
-		} catch (Exception e) {
-			// Note: In case of an exception, the framework may have sleeping threads (e.g. 'felix resolver')
-			// that only die after a minute. We don't want to wait so force exit.
-			e.printStackTrace();
-			System.exit(0);
-		}
-	}
+                        ServiceReference<GreetingService> sr = context.getServiceReference(GreetingService.class);
+                        if (sr == null) {
+                            throw new RuntimeException("Service reference is null");
+                        }
+                        GreetingService service = context.getService(sr);
+                        String msg = service.getGreetingMessage("Anne");
+                        System.out.println("RESULT: " + msg);
+                    } finally {
+                        bundle.stop();
+                        bundle.uninstall();
+                    }
+                }
+            } finally {
+                framework.stop();
+                framework.waitForStop(0);
+            }
+            System.out.println("Done.");
+        } catch (Exception e) {
+            // Note: In case of an exception, the framework may have sleeping threads (e.g. 'felix resolver')
+            // that only die after a minute. We don't want to wait so force exit.
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
 }
